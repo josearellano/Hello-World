0deac8ccb47981b8114774014de876f93ad8d3a2
Jose Arellano <arellano.jose@gmail.com>
2011-10-21 18:25:12 -0500

    bb<

---
+++ Prueba/Prueba/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  Prueba
+//
+//  Created by José Arellano P on 21/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Prueba/Prueba/PRViewController.h
@@ -0,0 +1,13 @@
+//
+//  PRViewController.h
+//  Prueba
+//
+//  Created by José Arellano P on 21/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRViewController : UIViewController
+
+@end
