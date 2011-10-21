c7ee1d48ceb3fa9372da7bbeaac2d15a14f0c71f
Jose Arellano <arellano.jose@gmail.com>
2011-10-21 18:25:33 -0500

    a

---
+++ Prueba/PruebaTests/PruebaTests.h
@@ -0,0 +1,13 @@
+//
+//  PruebaTests.h
+//  PruebaTests
+//
+//  Created by José Arellano P on 21/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PruebaTests : SenTestCase
+
+@end
---
+++ Prueba/PruebaTests/PruebaTests.m
@@ -0,0 +1,32 @@
+//
+//  PruebaTests.m
+//  PruebaTests
+//
+//  Created by José Arellano P on 21/10/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PruebaTests.h"
+
+@implementation PruebaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PruebaTests");
+}
+
+@end
